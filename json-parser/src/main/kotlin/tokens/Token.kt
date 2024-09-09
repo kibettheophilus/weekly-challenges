331d86da990cef82debd52cf37c4bcba2ab22e80
theophiluskibet <kibettheophilus@gmail.com>
2024-09-09 12:43:28 +0300

    add tokens

---
+++ json-parser/src/main/kotlin/tokens/Token.kt
@@ -0,0 +1,6 @@
+package tokens
+
+data class Token(
+    val type: TokenType,
+    val value: String
+)
---
+++ json-parser/src/main/kotlin/tokens/TokenType.kt
@@ -0,0 +1,15 @@
+package tokens
+
+enum class TokenType {
+    BraceOpen,
+    BraceClose,
+    BracketOpen,
+    BracketClose,
+    Number,
+    Comma,
+    Colon,
+    True,
+    False,
+    Null,
+    JsonQuote
+}
