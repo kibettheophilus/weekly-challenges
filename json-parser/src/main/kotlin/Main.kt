2cdbb12401a027f8c922fa5748462288875766e2
theophiluskibet <kibettheophilus@gmail.com>
2024-09-09 12:44:00 +0300

    setup lexer

--- json-parser/src/main/kotlin/Main.kt
+++ json-parser/src/main/kotlin/Main.kt
@@ -1,3 +1,22 @@
+import tokens.Token
+import tokens.TokenType
+
 fun main() {
     println("Hello World!")
+}
+
+fun lexer(input: String): List<Token> {
+    var current = 0
+    val tokens = mutableListOf<Token>()
+
+    while (current < input.length) {
+        val token = input[current]
+        if (token == '{') {
+            tokens.add(Token(type = TokenType.BraceOpen, value = token.toString()))
+            current++
+        } else if (token == '}') {
+            tokens.add(Token(type = TokenType.BraceClose, value = token.toString()))
+        }
+    }
+    return tokens
 }
