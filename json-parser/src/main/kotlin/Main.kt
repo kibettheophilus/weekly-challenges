09d2d30c38ded542ec482cda94f207e810ed583a
theophiluskibet <kibettheophilus@gmail.com>
2024-09-21 21:59:07 +0300

    update lexer

--- json-parser/src/main/kotlin/Main.kt
+++ json-parser/src/main/kotlin/Main.kt
@@ -10,12 +10,44 @@ fun lexer(input: String): List<Token> {
     val tokens = mutableListOf<Token>()
 
     while (current < input.length) {
-        val token = input[current]
+        var token = input[current]
+
         if (token == '{') {
             tokens.add(Token(type = TokenType.BraceOpen, value = token.toString()))
             current++
+            continue
         } else if (token == '}') {
             tokens.add(Token(type = TokenType.BraceClose, value = token.toString()))
+            current++
+            continue
+        } else if (token == '[') {
+            tokens.add(Token(type = TokenType.BracketOpen, value = token.toString()))
+            current++
+            continue
+        } else if (token == ']') {
+            tokens.add(Token(type = TokenType.BracketClose, value = token.toString()))
+            current++
+            continue
+        } else if (token == ':') {
+            tokens.add(Token(type = TokenType.Colon, value = token.toString()))
+            current++
+            continue
+        } else if (token == ',') {
+            tokens.add(Token(type = TokenType.Comma, value = token.toString()))
+            current++
+            continue
+        } else if (token == '"') {
+            var value = ""
+            token = input[++current]
+            while (token != '"') {
+                value += token
+                token = input[++current]
+            }
+            current++
+            tokens.add(Token(type = TokenType.BraceClose, value = token.toString()))
+            continue
+        }else {
+            println("Didnt find: $token")
         }
     }
     return tokens
